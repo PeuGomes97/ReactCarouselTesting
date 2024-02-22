f72719262d4aa457e0aa3281338baca17ea44b94
PeuGomes97 <peugomes97@gmail.com>
2024-02-22 00:23:45 -0300

    Caroulsel Testing concept

---
+++ src/Carousel.test.js
@@ -0,0 +1,52 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import Card from "./Card"
+import TEST_IMAGES from "./_testCommon.js";
+import '@testing-library/jest-dom/extend-expect';
+
+it ("renders without crashing", function(){
+  render(<Card/>)
+});
+
+it ("renders without crashing", function(){
+  render(<Carousel/>)
+});
+
+it ("matches snapshot", function(){
+  const {asFragment} = render(<Card/>);
+  expect(asFragment()).toMatchSnapshot();
+})
+
+it ("matches snapshot", function(){
+  const {asFragment} = render(<Carousel/>);
+  expect(asFragment()).toMatchSnapshot();
+})
+
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
